b70c043571b8de54160d3f1dbfb796d4059959e0
Tosic <toshestefan@gmail.com>
2023-08-18 17:15:14 +0200

    k3d

--- src/main/java/mk/finki/ukim/mk/lab/config/WebSecurityConfig.java
+++ src/main/java/mk/finki/ukim/mk/lab/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package mk.finki.ukim.mk.lab.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
